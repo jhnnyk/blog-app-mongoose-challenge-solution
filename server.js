918e8eb1859b8efffd4cb39e5c198c7f4425ae88
John Knotts <jhnnyk@gmail.com>
2017-09-19 12:51:14 -0600

    first working test on the GET endpoint

--- server.js
+++ server.js
@@ -1,49 +1,48 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
 
-const {DATABASE_URL, PORT} = require('./config');
-const {BlogPost} = require('./models');
+const {DATABASE_URL, PORT} = require('./config')
+const {BlogPost} = require('./models')
 
-const app = express();
+const app = express()
 
-app.use(morgan('common'));
-app.use(bodyParser.json());
+app.use(morgan('common'))
+app.use(bodyParser.json())
 
-mongoose.Promise = global.Promise;
-
+mongoose.Promise = global.Promise
 
 app.get('/posts', (req, res) => {
   BlogPost
     .find()
     .then(posts => {
-      res.json(posts.map(post => post.apiRepr()));
+      res.json({posts: posts.map(post => post.apiRepr())})
     })
     .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-    });
-});
+      console.error(err)
+      res.status(500).json({error: 'something went terribly wrong'})
+    })
+})
 
 app.get('/posts/:id', (req, res) => {
   BlogPost
     .findById(req.params.id)
     .then(post => res.json(post.apiRepr()))
     .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went horribly awry'});
-    });
-});
+      console.error(err)
+      res.status(500).json({error: 'something went horribly awry'})
+    })
+})
 
 app.post('/posts', (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
+  const requiredFields = ['title', 'content', 'author']
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i]
     if (!(field in req.body)) {
       const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
+      console.error(message)
+      return res.status(400).send(message)
     }
   }
 
@@ -55,106 +54,101 @@ app.post('/posts', (req, res) => {
     })
     .then(blogPost => res.status(201).json(blogPost.apiRepr()))
     .catch(err => {
-        console.error(err);
-        res.status(500).json({error: 'Something went wrong'});
-    });
-
-});
-
+      console.error(err)
+      res.status(500).json({error: 'Something went wrong'})
+    })
+})
 
 app.delete('/posts/:id', (req, res) => {
   BlogPost
     .findByIdAndRemove(req.params.id)
     .then(() => {
-      res.status(204).json({message: 'success'});
+      res.status(204).json({message: 'success'})
     })
     .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-    });
-});
-
+      console.error(err)
+      res.status(500).json({error: 'something went terribly wrong'})
+    })
+})
 
 app.put('/posts/:id', (req, res) => {
   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     res.status(400).json({
       error: 'Request path id and request body id values must match'
-    });
+    })
   }
 
-  const updated = {};
-  const updateableFields = ['title', 'content', 'author'];
+  const updated = {}
+  const updateableFields = ['title', 'content', 'author']
   updateableFields.forEach(field => {
     if (field in req.body) {
-      updated[field] = req.body[field];
+      updated[field] = req.body[field]
     }
-  });
+  })
 
   BlogPost
     .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
     .then(updatedPost => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Something went wrong'}));
-});
-
+    .catch(err => res.status(500).json({message: 'Something went wrong'}))
+})
 
 app.delete('/:id', (req, res) => {
   BlogPosts
     .findByIdAndRemove(req.params.id)
     .then(() => {
-      console.log(`Deleted blog post with id \`${req.params.ID}\``);
-      res.status(204).end();
-    });
-});
+      console.log(`Deleted blog post with id \`${req.params.ID}\``)
+      res.status(204).end()
+    })
+})
 
-
-app.use('*', function(req, res) {
-  res.status(404).json({message: 'Not Found'});
-});
+app.use('*', function (req, res) {
+  res.status(404).json({message: 'Not Found'})
+})
 
 // closeServer needs access to a server object, but that only
 // gets created when `runServer` runs, so we declare `server` here
 // and then assign a value to it in run
-let server;
+let server
 
 // this function connects to our database, then starts the server
-function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+function runServer (databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, err => {
       if (err) {
-        return reject(err);
+        return reject(err)
       }
       server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
+        console.log(`Your app is listening on port ${port}`)
+        resolve()
       })
       .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
-    });
-  });
+        mongoose.disconnect()
+        reject(err)
+      })
+    })
+  })
 }
 
 // this function closes the server, and returns a promise. we'll
 // use it in our integration tests later.
-function closeServer() {
+function closeServer () {
   return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
+    return new Promise((resolve, reject) => {
+      console.log('Closing server')
+      server.close(err => {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+  })
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  runServer().catch(err => console.error(err))
 };
 
-module.exports = {runServer, app, closeServer};
+module.exports = {runServer, app, closeServer}
--- test/test-blogposts-integration.js
+++ test/test-blogposts-integration.js
@@ -10,3 +10,72 @@ const {app, runServer, closeServer} = require('../server')
 const {TEST_DATABASE_URL} = require('../config')
 
 chai.use(chaiHttp)
+
+// add test data to the test database
+function seedBlogPostData () {
+  console.info('seeding blog post data...')
+  const seedData = []
+
+  for (let i = 1; i <= 10; i++) {
+    seedData.push(generateBlogPostData())
+  }
+
+  return BlogPost.insertMany(seedData)
+}
+
+function generateBlogPostData () {
+  return {
+    author: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName()
+    },
+    title: faker.lorem.words(),
+    content: faker.lorem.paragraphs(),
+    created: faker.date.past()
+  }
+}
+
+function tearDownDb () {
+  console.warn('deleting database...')
+  return mongoose.connection.dropDatabase()
+}
+
+describe('BlogPost API resource', function () {
+  // before running the tests, connect to the test database and start the server
+  before(function () {
+    return runServer(TEST_DATABASE_URL)
+  })
+
+  // before each test, seed the test database
+  beforeEach(function () {
+    return seedBlogPostData()
+  })
+
+  // after each test, destroy the test database
+  afterEach(function () {
+    return tearDownDb()
+  })
+
+  // after running the tests, stop the server
+  after(function () {
+    return closeServer()
+  })
+
+  describe('GET endpoint', function () {
+    it('should return all existing blog posts', function () {
+      let res
+      return chai.request(app)
+        .get('/posts')
+        .then(function (_res) {
+          res = _res
+          res.should.have.status(200)
+          res.body.posts.should.have.length.of.at.least(1)
+          return BlogPost.count()
+        })
+        .then(function (count) {
+          console.log(count)
+          res.body.posts.should.have.lengthOf(count)
+        })
+    })
+  })
+})
